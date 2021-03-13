832218e25a3376e31f7d6bbb8881894bcf144769
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-03-14 00:40:33 +0530

    [Shubham] Initializing the components

--- jsr-messenger/src/App.js
+++ jsr-messenger/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={Home}/>
+        <Route path="/" exact component={Home}/>
         <Route path="/login" component={Login}/>
         <Route path="/register" component={Register}/>
       </Router>
